550bd3848bfe808e292185b34b0cc1931a70b11b
Nicolas Silva <nicolasfernando616@gmail.com>
2026-03-21 23:47:24 -0300

    v-final

--- TCPServer.py
+++ TCPServer.py
@@ -1,19 +1,20 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print("O servidor está ativo")
 while True:
-    connectionSocket, adrr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept()
+    print("Conexão estabelecida com", addr) 
     while True:
-     sentence = connectionSocket.recv(1024).decode()
-     if sentence == 'FIM':
-            sentence = "Conexao encerrada"
-            connectionSocket.send(sentence.encode())
+        sentence = connectionSocket.recv(1024).decode()
+        print("Mensagem recebida:", sentence)
+        if sentence == 'FIM':
+            connectionSocket.send("Conexao encerrada".encode())
+            print("Conexão encerrada")
             break
-     else:
-      capitalizedSentence = sentence.upper()
-      connectionSocket.send(capitalizedSentence.encode())
-
+        else:
+            connectionSocket.send("Mensagem recebida com sucesso".encode())
     connectionSocket.close()
